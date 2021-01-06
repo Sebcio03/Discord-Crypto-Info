14b1d3a19043eca70876420b6e8a3e6c305d39c2
Sebastian Ozimkowski <sebastianozimkowski@outlook.com>
2021-01-06 22:08:58 +0100

    adding ETH, modify formating

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 BITCOIN_URL = 'https://api.blockchain.com/v3/exchange/tickers/BTC-USD'
-# Free exchange api ;)
+ETHERENUM_URL = 'https://api.blockchain.com/v3/exchange/tickers/ETH-USD'
 EXCHANGE_URL = 'https://free.currconv.com/api/v7/convert?q=USD_PLN&compact=ultra&apiKey=[api_key]'
 WEBHOOK_URL = ''
 
@@ -41,35 +41,48 @@ def get_uno_price():
         "https://www.olx.pl/oferty/q-fiat-uno/?search%5Border%5D=filter_float_price%3Adesc&search%5Bfilter_float_price%3Ato%5D=10000")
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    price = soup.select_one(
-        ".space.inlblk.rel strong").text
+    price = soup.select_one(".space.inlblk.rel strong").text
     price = eval(price.replace(
         " zł", "").replace(",", ".").replace(" ", ""))
 
     return price
 
 
-bitcoin_price = requests.get(BITCOIN_URL).json().get("price_24h")
-if not bitcoin_price:
-    exit()
+pasztetowa_price = get_pasztetowa_price()
+kozaki_price = get_kozaki_price()
+uno_price = get_uno_price()
 
+btc_price = requests.get(BITCOIN_URL).json().get("price_24h")
+eth_price = requests.get(ETHERENUM_URL).json().get("price_24h")
 usd2pln_price = requests.get(EXCHANGE_URL).json().get("USD_PLN")
-if not usd2pln_price:
-    exit()
 
-bitcoin2pln = bitcoin_price * usd2pln_price
+btc2pln = btc_price * usd2pln_price
+eth2pln = eth_price * usd2pln_price
+
+btc_kozaki_count = btc2pln / kozaki_price
+btc_pasztetowa_count = btc2pln / pasztetowa_price
+btc_uno_count = btc2pln / uno_price
+eth_kozaki_count = eth2pln / kozaki_price
+eth_pasztetowa_count = eth2pln / pasztetowa_price
+eth_uno_count = eth2pln / uno_price
+
 date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-kozaki_count = bitcoin2pln // get_kozaki_price()
-pasztetowa_count = bitcoin2pln // get_pasztetowa_price()
-uno_count = bitcoin2pln // get_uno_price()
 
 msg = f"""```md
 # Dzień dobry! dziś mamy {date}
-Bitcoin kosztuje <{round(bitcoin2pln,0)}PLN> tj.
 
-[1]: {kozaki_count} par kozaków
-[2]: {pasztetowa_count} kg pasztetowej
-[3]: {uno_count} fiatów uno
+Bitcoin kosztuje {btc2pln:08,.2f} PLN tj.
+
+[1]: {btc_kozaki_count:01,.1f} par kozaków
+[2]: {btc_pasztetowa_count:01,.1f} kg pasztetowej
+[3]: {btc_uno_count:01,.1f} fiatów uno
+
+
+Etherenum kosztuje {eth2pln:01,.1f} PLN tj.
+
+[1]: {eth_kozaki_count:01,.1f} par kozaków
+[2]: {eth_pasztetowa_count:01,.1f} kg pasztetowej
+[3]: {eth_uno_count:01,.1f} fiatów uno
 ```"""
 
 r = requests.post(WEBHOOK_URL, json={"content": msg})
